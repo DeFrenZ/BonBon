3cfb493bacc18346ea0a0407d2a4378eb600df46
DeFrenZ <defrenz@me.com>
2017-02-06 13:21:37 +0000

    Add option to allow concurrent reads
    Change the type of lock internally used to allow concurrent reads when the flag
    is passed.

--- Sources/Synchronized.swift
+++ Sources/Synchronized.swift
@@ -2,12 +2,20 @@ public final class Synchronized<Wrapped> {
 	private var _lock: Lock
 	private var _value: Wrapped
 
-	public init(_ value: Wrapped) {
+	public init(_ value: Wrapped, allowConcurrentReads: Bool = true) {
 		self._value = value
+		self._lock = allowConcurrentReads ? ReadWriteLock() : MutexLock()
+	}
+
+	private func readSync <T> (_ perform: () throws -> T) rethrows -> T {
+		guard let concurrentLock = _lock as? ConcurrentLock else {
+			return try _lock.sync(perform)
+		}
+		return try concurrentLock.concurrentSync(perform)
 	}
 
 	public var value: Wrapped {
-		get { return _lock.sync { _value } }
+		get { return readSync { _value } }
 		set { _lock.sync { _value = newValue } }
 	}
 
