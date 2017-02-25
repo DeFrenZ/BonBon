bfb3f396ac0b993ce55204e75cb3d2db5e3e26f6
DeFrenZ <defrenz@me.com>
2017-02-25 21:55:45 +0000

    Add docs
    Add documentation to the whole `Box.swift` file.

--- Sources/Box.swift
+++ Sources/Box.swift
@@ -1,36 +1,85 @@
+/// A protocol representing the common interface of reference wrappers around
+/// values. This protocol is only used to share implementation between the
+/// available box types.
 protocol Box: class {
+	/// The type of the wrapped value.
+	///	- warning: Using a reference type defeats the purpose of this wrapper.
 	associatedtype Wrapped
-	init(wrapping: Wrapped)
+
+	/// Initialize a box wrapping the given value.
+	///
+	///	- parameter value: The value to wrap.
+	///	- returns: A reference wrapper holding the given value.
+	init(wrapping value: Wrapped)
+
+	/// Retrieve the wrapped value by "unboxing" it.
 	var value: Wrapped { get }
 }
 
 extension Box {
+	/// Create a new box wrapping the same value. Suggested when handling a
+	/// `MutableBox` that shouldn't mutate anymore.
+	///
+	///	- returns: A new `ImmutableBox` wrapping the same value as `self`.
 	public func copy() -> ImmutableBox<Wrapped> {
 		return .init(wrapping: value)
 	}
+
+	/// Create a new mutable box wrapping the same value. Suggested when
+	/// handling an `ImmutableBox` which you want to mutate.
+	///
+	///	- returns: A new `MutableBox` wrapping the same value as `self`.
 	public func mutableCopy() -> MutableBox<Wrapped> {
 		return .init(wrapping: value)
 	}
 }
 
 extension Box {
-	public func map <T> (_ transform: (Wrapped) throws -> T) rethrows -> ImmutableBox<T> {
+	/// Create a new box wrapping the value resulting from applying the given
+	/// transform to the wrapped one.
+	///
+	///	- parameter transform: The transform to apply on the wrapped value.
+	///	- parameter value: The value to transform.
+	///	- returns: A new `ImmutableBox` wrapping the transformed value.
+	public func map <T> (_ transform: (_ value: Wrapped) throws -> T) rethrows -> ImmutableBox<T> {
 		return try .init(wrapping: transform(value))
 	}
-	public func flatMap <NewBox: Box> (_ transform: (Wrapped) throws -> NewBox) rethrows -> ImmutableBox<NewBox.Wrapped> {
+
+	/// Create a new box wrapping the same value as in the box resulting from
+	/// applying the given transform to the wrapped one.
+	///
+	///	- parameter transform: The transform to apply on the wrapped value.
+	///	- parameter value: The value to transform.
+	///	- returns: A new `ImmutableBox` wrapping the value wrapped by the
+	///		transform result.
+	public func flatMap <NewBox: Box> (_ transform: (_ value: Wrapped) throws -> NewBox) rethrows -> ImmutableBox<NewBox.Wrapped> {
 		return try map({ try transform($0).value })
 	}
 }
 
+/// A `Box` that doesn't change the value it wraps after instantiation.
 public final class ImmutableBox<Wrapped>: Box {
+	/// The wrapped value.
 	public let value: Wrapped
+
+	/// Create a new box wrapping the given value.
+	///
+	///	- parameter value: The value to wrap.
+	///	- returns: A box wrapping the value.
 	public init(wrapping value: Wrapped) {
 		self.value = value
 	}
 }
 
+/// A `Box` that can change the value it wraps after instantiation.
 public final class MutableBox<Wrapped>: Box {
+	/// The currently wrapped value.
 	public var value: Wrapped
+
+	/// Create a new mutable box wrapping the given value.
+	///
+	///	- parameter value: The value to initially wrap.
+	///	- returns: A mutable box wrapping the value.
 	public init(wrapping value: Wrapped) {
 		self.value = value
 	}
