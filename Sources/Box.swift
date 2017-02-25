86d3fa3bb3449c592d3e362a6f6f1c35a5971556
DeFrenZ <defrenz@me.com>
2017-02-25 16:37:16 +0000

    Make `Box` a protocol
    Make `Box` a protocol to share functionality between the mutable and immutable
    variants without resorting to subclassing, which can always hold surprises.

--- Sources/Box.swift
+++ Sources/Box.swift
@@ -1,13 +1,19 @@
-public final class Box<Wrapped> {
+protocol Box: class {
+	associatedtype Wrapped
+	init(wrapping: Wrapped)
+	var value: Wrapped { get }
+}
+
+public final class ImmutableBox<Wrapped>: Box {
 	public let value: Wrapped
-	public init(_ value: Wrapped) {
+	public init(wrapping value: Wrapped) {
 		self.value = value
 	}
 }
 
-public final class MutableBox<Wrapped> {
+public final class MutableBox<Wrapped>: Box {
 	public var value: Wrapped
-	public init(_ value: Wrapped) {
+	public init(wrapping value: Wrapped) {
 		self.value = value
 	}
 }
