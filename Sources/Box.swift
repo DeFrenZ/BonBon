ef15392e3779d660853a12fd7bb0b63ce87d98e4
DeFrenZ <defrenz@me.com>
2017-02-25 16:38:17 +0000

    Add copies
    Add methods for copying the box. Both immutable and mutable versions.

--- Sources/Box.swift
+++ Sources/Box.swift
@@ -4,6 +4,15 @@ protocol Box: class {
 	var value: Wrapped { get }
 }
 
+extension Box {
+	public func copy() -> ImmutableBox<Wrapped> {
+		return .init(wrapping: value)
+	}
+	public func mutableCopy() -> MutableBox<Wrapped> {
+		return .init(wrapping: value)
+	}
+}
+
 public final class ImmutableBox<Wrapped>: Box {
 	public let value: Wrapped
 	public init(wrapping value: Wrapped) {
