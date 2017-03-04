6139b3f43b19207f0a3582ab6243b5bbba8bb410
DeFrenZ <defrenz@me.com>
2017-03-04 21:12:06 +0000

    Make `Validator` nested
    Move the `typealias` inside `Validated` and the `validate` helper function as
    well, to avoid much unnecessary repletion of the `Value` generic type and for
    better context.

--- Sources/Validated.swift
+++ Sources/Validated.swift
@@ -1,29 +1,29 @@
 public struct Validated<Value> {
-	public private(set) var value: Value
-	private var validator: Validator<Value>
+	public typealias Validator = (Value) throws -> Void
 
-	public init(value: Value, validator: @escaping Validator<Value>) throws {
-		self.value = try validated(value, with: validator)
+	public private(set) var value: Value
+	private var validator: Validator
+
+	private static func validated(_ value: Value, with validator: Validator) throws -> Value {
+		do {
+			try validator(value)
+			return value
+		} catch {
+			throw ValidationError(invalidValue: value, validatorError: error)
+		}
+	}
+
+	public init(value: Value, validator: @escaping Validator) throws {
+		self.value = try Validated.validated(value, with: validator)
 		self.validator = validator
 	}
 
 	public mutating func set(to newValue: Value) throws {
-		value = try validated(newValue, with: validator)
+		value = try Validated.validated(newValue, with: validator)
 	}
 }
-
-public typealias Validator<Value> = (Value) throws -> Void
 
 public struct ValidationError<Value>: Error {
 	public var invalidValue: Value
 	public var validatorError: Error
-}
-
-private func validated <Value> (_ value: Value, with validator: Validator<Value>) throws -> Value {
-	do {
-		try validator(value)
-		return value
-	} catch {
-		throw ValidationError(invalidValue: value, validatorError: error)
-	}
 }
